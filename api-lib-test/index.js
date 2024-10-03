c87c0ecbb828a75980b9145a8617702a380496c1
Dominique Parisot <powimod@gmail.com>
2024-10-03 05:54:47 +0000

    Add communication with backend in api-lib

--- api-lib-test/index.js
+++ api-lib-test/index.js
@@ -1,7 +1,6 @@
 'use strict'
 import dotenv from 'dotenv'
 
-//import { checkApiLib } from 'comaint-api-lib'
 import { ComaintBackendApi } from 'comaint-api-lib'
 
 dotenv.config()
@@ -11,18 +10,44 @@ const main = async () => {
     if (! backendUrl)
         throw new Error('backendUrl not defined')
     const api = new ComaintBackendApi(backendUrl)
-    const ret = api.checkApiLib()
+
+    console.log("Checking API library...")
+    let ret = api.checkApiLib()
     if (! ret.success)
         throw new Error(ret.message)
-    console.log(ret.message)
+    console.log(`\t→ ${ret.message}`)
 
+    console.log("Checking backend communication...")
+    ret = await api.checkBackend()
+    if (! ret.success)
+        throw new Error(ret.message)
+    console.log(`\t→ ${ret.message}`)
+
+    console.log("Search API version...")
+    ret = await api.getApiVersion()
+    if (! ret.success)
+        throw new Error(ret.message)
+    console.log(`\t→ API version : ${ret.version}`)
+   
+    console.log("Search backend version...")
+    ret = await api.getBackendVersion()
+    if (! ret.success)
+        throw new Error(ret.message)
+    console.log(`\t→ backend version : ${ret.version}`)
+ 
+    console.log("Check error handling...")
+    ret = await api.welcome({}) // missing arguments «firstname» and «lastname» in request
+    if (ret.success) 
+        throw new Error('Error handling is not workinkg')
+    console.log(`\t→ done`)
+
+
+    console.log('End')
 }
 
 main()
-/* TODO reactivate this
 . catch (error => {
 	const message = error.message ? error.message : error
     console.error(`ERROR : ${message}`)
 	process.exit(1)
 })
-*/
--- api-lib/src/ComaintBackendApi.js
+++ api-lib/src/ComaintBackendApi.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import { jsonGet, jsonPost } from './util.js'
+
 class ComaintBackendApi {
 
     #backendUrl = null
@@ -12,7 +14,61 @@ class ComaintBackendApi {
 
 
     checkApiLib() {
-        return { success: true, message: 'Comaint api-lib is working !'}
+        return {success: true, message: 'Comaint api-lib is working !'}
+    }
+
+    async checkBackend() {
+        const API_VERSION_ROUTE = '/api/welcome'
+        try {
+            const ret = await jsonGet(this.#backendUrl, API_VERSION_ROUTE)
+            return {success: true, message: 'Comaint backend communication is working !'}
+        }
+        catch (error) {
+            const message = error.message === undefined ? error : error.message
+            return {success: false, message: `Can't communicate with backend ${this.#backendUrl} (${message})`}
+        }
+    }
+
+    async getApiVersion() {
+        const API_VERSION_ROUTE = '/api/version'
+        // TODO catch errors
+        let json = await jsonGet(this.#backendUrl, API_VERSION_ROUTE)
+        return {success: true, version: json.version}
+    }
+
+    async getBackendVersion() {
+        const BACKEND_VERSION_ROUTE = '/api/v1/backend-version'
+        let json = await jsonGet(this.#backendUrl, BACKEND_VERSION_ROUTE)
+        return {success: true, version: json.version}
+    }
+
+
+    async welcome(args) {
+        const WELCOME_ROUTE = '/api/welcome'
+        try  {
+            let ret
+            if (args !== undefined) {
+                if (typeof(args) !== 'object')
+                    throw new Error('Argument is not an object')
+                /*
+                if (! args.firstname)
+                    throw new Error('Argument «firstname» not defined')
+                if (! args.lastname)
+                    throw new Error('Argument «firstname» not defined')
+                */
+                ret = await jsonPost(this.#backendUrl, WELCOME_ROUTE, args)
+            }
+            else {
+                ret = await jsonGet(this.#backendUrl, WELCOME_ROUTE)
+            }
+            console.log("dOm retour", ret)
+            return {success: true, message: ret.message} 
+        }
+        catch (error) {
+            const message = error.message === undefined ? error : error.message
+            return {success: false, message }
+        }
+
     }
 
 }
