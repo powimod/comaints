e68e230c10d475cafcf5e4539a4046f0810aff4f
Dominique Parisot <powimod@gmail.com>
2024-10-06 15:30:11 +0000

    Add non-functional login route

--- backend/src/routes/AuthRoutes.js
+++ backend/src/routes/AuthRoutes.js
@@ -152,6 +152,44 @@ class AuthRoutes {
                 view.error(error)
             }
         })
+        // public route 
+        expressApp.post('/api/v1/auth/login', async (request, response) => {
+            const view = new View(request, response)
+            try {
+                let email = request.body.email
+                if (email === undefined)
+                    throw new ComaintApiErrorInvalidRequest('error.request_param_not_found', { parameter: 'email'})
+                if (typeof(email) !== 'string')
+                    throw new ComaintApiErrorInvalidRequest('error.request_param_invalid', { parameter: 'email'})
+                const [ errorMsg1, errorParam1 ] = controlObjectProperty(userObjectDef, 'email', email) 
+                if (errorMsg1) 
+                    throw new ComaintApiErrorInvalidRequest(errorMsg1, errorParam1)
+
+                let password = request.body.password
+                if (password === undefined)
+                    throw new ComaintApiErrorInvalidRequest('error.request_param_not_found', { parameter: 'password'})
+                if (typeof(password) !== 'string')
+                    throw new ComaintApiErrorInvalidRequest('error.request_param_invalid', { parameter: 'password'})
+                // FIXME strange error «Cannot set properties of undefined (setting 'undefined')»
+                // let errorMsg, errorParam
+                // First call :
+                //      [ errorMsg, errorParam ] = controlObjectProperty(userObjectDef, 'password', password) 
+                //      => no error
+                // Second call :
+                //      [ errorMsg, errorParam ] = controlObjectProperty(userObjectDef, 'password', password) 
+                //      => Error «Cannot set properties of undefined (setting 'undefined')»
+                const [ errorMsg2, errorParam2 ] = controlObjectProperty(userObjectDef, 'password', password) 
+                if (errorMsg2) 
+                    throw new ComaintApiErrorInvalidRequest(errorMsg2, errorParam2)
+            }
+            catch(error) {
+                view.error(error)
+            }
+
+
+        })
+
+
     }
 }
 
---
+++ backend/test/test04-login.js
@@ -0,0 +1,158 @@
+'use strict'
+import { expect } from 'chai'
+import assert from 'assert'
+
+import { loadConfig, jsonGet, jsonPost, connectDb, disconnectDb, requestDb } from './util.js'
+
+const ROUTE_LOGIN= 'api/v1/auth/login'
+
+describe('Test user registration', () => {
+
+    const dte = new Date()
+    const userEmail = `u${dte.getTime()}@x.y`
+    let validationCode  = null
+    let accessToken = null
+    let refreshToken = null
+    let userId = null
+
+    before( async () =>  {
+        loadConfig()
+        await connectDb()
+    }),
+
+    after( async () =>  {
+        await requestDb('DELETE FROM users WHERE email=?', userEmail)
+        await disconnectDb()
+    }),
+
+    describe(`Call route /${ROUTE_LOGIN} with invalid data`, () => {
+
+        it(`Should detect missing email in request body`, async () => {
+            try {
+                let json = await jsonPost(ROUTE_LOGIN, {
+                        email_missing:'',
+                        password:''
+                    })
+                expect.fail("Missing «email» parameter not detected")
+            }
+            catch (error) {
+                expect(error).to.be.instanceOf(Error)
+                expect(error.message).to.equal(`Server status 400 (Parameter «email» not found in request body)`)
+            }
+        })
+
+        it(`Should detect empty email in request body`, async () => {
+            try {
+                let json = await jsonPost(ROUTE_LOGIN, {
+                        email:'',
+                        password:''
+                    })
+                expect.fail("Missing «email» parameter not detected")
+            }
+            catch (error) {
+                expect(error).to.be.instanceOf(Error)
+                expect(error.message).to.equal(`Server status 400 (Property «email» is too short)`)
+            }
+        })
+
+        it(`Should detect malformed email in request body`, async () => {
+            try {
+                let json = await jsonPost(ROUTE_LOGIN, {
+                        email:'abcdef',
+                        password:''
+                    })
+                expect.fail("Missing «email» parameter not detected")
+            }
+            catch (error) {
+                expect(error).to.be.instanceOf(Error)
+                expect(error.message).to.equal(`Server status 400 (Property «email» is not a valid email)`)
+            }
+        })
+
+        it('Should detect missing password in request body', async () => {
+            try {
+                let json = await jsonPost(ROUTE_LOGIN, {
+                        email:'a@b.c',
+                        password_missing:'',
+                        sendCodeByEmail: false
+                    })
+                expect.fail('Missing «password» parameter not detected')
+            }
+            catch (error) {
+                expect(error).to.be.instanceOf(Error)
+                expect(error.message).to.equal('Server status 400 (Parameter «password» not found in request body)')
+            }
+        })
+
+
+        it('Should detect too small password in request body', async () => {
+            try {
+                let json = await jsonPost(ROUTE_LOGIN, {
+                        email:'a@b.c',
+                        password:'abc',
+                        sendCodeByEmail: false
+                    })
+                expect.fail('Invalid «password» parameter not detected')
+            }
+            catch (error) {
+                expect(error).to.be.instanceOf(Error)
+                expect(error.message).to.equal('Server status 400 (Password is too small)')
+            }
+        })
+
+        it('Should detect password with no uppercase letter in request body', async () => {
+            try {
+                let json = await jsonPost(ROUTE_LOGIN, {
+                        email:'a@b.c',
+                        password:'abcdefghijk9.',
+                        sendCodeByEmail: false
+                    })
+                expect.fail('Invalid «password» parameter not detected')
+            }
+            catch (error) {
+                expect(error).to.be.instanceOf(Error)
+                expect(error.message).to.equal('Server status 400 (Password does not contain uppercase letter)')
+            }
+        })
+
+        it('Should detect password with no digit character in request body', async () => {
+            try {
+                let json = await jsonPost(ROUTE_LOGIN, {
+                        email:'a@b.c',
+                        password:'aBcdefghijkl.',
+                        sendCodeByEmail: false
+                    })
+                expect.fail('Invalid «password» parameter not detected')
+            }
+            catch (error) {
+                expect(error).to.be.instanceOf(Error)
+                expect(error.message).to.equal('Server status 400 (Password does not contain digit character)')
+            }
+        })
+
+        it('Should detect password with no special character in request body', async () => {
+            try {
+                let json = await jsonPost(ROUTE_LOGIN, {
+                        email:'a@b.c',
+                        password:'aBcdefghijkl9',
+                        sendCodeByEmail: false
+                    })
+                expect.fail('Invalid «password» parameter not detected')
+            }
+            catch (error) {
+                expect(error).to.be.instanceOf(Error)
+                expect(error.message).to.equal('Server status 400 (Password does not contain special character)')
+            }
+        })
+
+    })
+
+    /*
+    describe(`Call route /${ROUTE_LOGIN} with valid data`, () => {
+        //TODO check incorrect password
+    })
+    */
+
+})
+
+
