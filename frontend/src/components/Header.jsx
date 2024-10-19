5a18e68a536c8f0d7a6fe30c7b9ef742acde8701
Dominique Parisot <powimod@gmail.com>
2024-10-19 18:02:31 +0200

    Add i18n support

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,16 @@
+import i18n from "i18next";
+import { useTranslation } from 'react-i18next';
+
+
 import '../scss/header.scss'
 
 const Header = () => {
+    const { t } = useTranslation()
+
     return (
         <header className="root_header">
-            <h1>Comaint</h1>
+            <h1>{t('header_title')}</h1>
+            <h2>{t('header_subtitle')}</h2>
         </header>
     )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
 
+import './i18n'
 import createRouter from './router'
 
 createRoot(document.getElementById('root')).render(
