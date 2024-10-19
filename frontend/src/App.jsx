2cf2ea498f9718c75e3e39959077a15c09e694f6
Dominique Parisot <powimod@gmail.com>
2024-10-19 12:00:19 +0200

    Connect frontend to API-lib

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import { useEffect, useState } from 'react'
+import api from './api.js'
+
+
 function App() {
 
-  return (
-    <>
-      <h1>Comaint</h1>
-    </>
-  )
+
+    const [versionBackend, setVersionBackend ]  = useState('?')
+
+    useEffect( () => {
+        const getBackendVersion = async () => {
+            const ret = await api.getBackendVersion()
+            setVersionBackend( ret.success ? ret.version : ret.message)
+        }
+        getBackendVersion()
+    }, [])
+
+    return (<>
+        <h1>Comaint</h1>
+        <p>Version : {versionBackend}</p>
+    </>)
 }
 
 export default App
