3d6ae771e6a4f8085fae01868d22e4f484cbc20a
Dominique Parisot <powimod@gmail.com>
2024-10-20 09:47:17 +0200

    Add StockIcon component

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -1,3 +1,4 @@
+import StockIcon from  '../components/StockIcon'
 import '../scss/home.scss'
 
 const Home = (props) => {
@@ -5,6 +6,13 @@ const Home = (props) => {
 	return (
 		<main className='page_home'>
             <h2>Comaint main page</h2>
+            <div>
+                <StockIcon icon='user' size="huge"/>
+                <StockIcon icon='order' size="large"/>
+                <StockIcon icon='unit' size="medium"/>
+                <StockIcon icon='equipment' size="small"/>
+                <StockIcon icon='supplier' size="tiny"/>
+            </div>
 		</main>
 	)
 }
