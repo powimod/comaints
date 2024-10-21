07ea52259c6a7fd866077fc21a2038544c1c35cc
Dominique Parisot <powimod@gmail.com>
2024-10-21 18:43:32 +0200

    Import Awesome symbols

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import i18n from "i18next";
 import { useTranslation } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faWheelchair } from '@fortawesome/free-solid-svg-icons'
 
 import Logo from './Logo'
 
@@ -9,11 +11,21 @@ import '../scss/header.scss'
 const Header = () => {
     const { t } = useTranslation()
 
+    const onAccessibilityButtonClick = () => {
+        console.log('accessibility')
+    }
+
     return (
         <header className="root_header">
             <Logo/>
             <h1>{t('header_title')}</h1>
             <h2>{t('header_subtitle')}</h2>
+
+            <FontAwesomeIcon className="accessibility-button" 
+                icon={faWheelchair} 
+                onClick={onAccessibilityButtonClick} 
+                inverse/>
+
         </header>
     )
 }
