731ab301080e86c212a9850cdcd7157e361342b4
Dominique Parisot <powimod@gmail.com>
2024-10-23 07:17:30 +0200

    Add About container

---
+++ frontend/src/Config.jsx
@@ -0,0 +1,6 @@
+const Config = {
+	'version': '0.0.1',
+	'website': 'https://github.com/powimod/comaint',
+	'contact': 'powimod@gmail.com'
+}
+export default Config
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import ErrorPage from './containers/ErrorPage.jsx'
 import Home from "./containers/Home"
 import DialogDemo from "./containers/DialogDemo"
+import About, {loader as aboutLoader} from "./containers/About"
 
 const createRouter = () => {
 	return createBrowserRouter([
@@ -14,6 +15,7 @@ const createRouter = () => {
 			children: [
 				{ index:true, element:<Home/> },
 				{ path: '/dialog-demo' , element:<DialogDemo/> },
+				{ path: '/about', element:<About/>, loader: aboutLoader }
 			]
 		}
 	])
