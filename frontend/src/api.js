16460fb4e8adc457a6064be3debae42c29c6a445
Dominique Parisot <powimod@gmail.com>
2024-10-27 19:03:28 +0100

    Add accountSerializeFunction

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,23 @@
 import { ComaintBackendApi } from 'comaint-api-lib'
 
+const accountSerializeFunction = (data) => {
+    const accountStorageKey = 'account'
+    if (data === undefined) {
+        // load account from local storage
+        const accountData = localStorage.getItem(accountStorageKey)
+        data = JSON.parse(accountData)
+    }
+    else {
+        // save account to local storage
+        const accountData = JSON.stringify(data)
+        localStorage.setItem(accountStorageKey, accountId)
+    }
+    return data
+}
+
+
+
 const apiUrl = window.location.origin
-const api = new ComaintBackendApi(apiUrl)
+const api = new ComaintBackendApi(apiUrl, accountSerializeFunction)
 
 export default api
