2ef6e9cb177c5eeb33a339c8cc950b8bc0835546
Dominique Parisot <powimod@gmail.com>
2024-10-27 09:36:33 +0100

    Add database helper in api-lib selftests

---
+++ api-lib/test/helpers.js
@@ -0,0 +1,85 @@
+'use strict'
+
+import { expect } from 'chai'
+
+import { requestDb } from './util.js'
+
+const ROUTE_REGISTER = 'api/v1/auth/register'
+const ROUTE_LOGOUT = 'api/v1/auth/logout'
+const ROUTE_VALIDATE = 'api/v1/auth/validate'
+
+const DEFAULT_PASSWORD = 'abC.dEf.GH1.lMn!'
+
+const createUserAccount = async (options = {}) => {
+    let {
+        email = null,
+        password = DEFAULT_PASSWORD,
+        logout = false
+    } = options
+
+    if (email === null) {
+        const dte = new Date()
+        email = `u${dte.getTime()}@x.y`
+    }
+
+	let json = await jsonPost(ROUTE_REGISTER, { email, password, sendCodeByEmail: false })
+
+    const res = await requestDb('select * from users where email=?', [ email ])
+    expect(res).to.be.instanceOf(Array)
+    const user = res[0]
+    expect(user).to.be.instanceOf(Object)
+    expect(user).to.have.property('id')
+    const authCode = user.auth_code
+
+    json = await jsonPost(ROUTE_VALIDATE, { code: authCode})
+    expect(json).to.be.instanceOf(Object)
+    expect(json).to.have.property('validated')
+    expect(json.validated).to.be.a('boolean').and.to.equal(true)
+
+    if (logout)
+        await jsonPost(ROUTE_LOGOUT)
+
+    return {
+        id: user.id,
+        email: user.email
+    }
+}
+
+const deleteUserAccount = async (user) => {
+    if (user === null || typeof(user) !== 'object')
+        return
+    await requestDb('DELETE FROM users WHERE id=?', user.id)
+}
+
+const getDatabaseUserByEmail = async (email) => {
+    const res = await requestDb('select * from users where email=?', [ email ])
+    expect(res).to.be.instanceOf(Array)
+    const user = res[0]
+    expect(user).to.be.instanceOf(Object)
+    expect(user).to.have.property('email')
+    expect(user.email).to.equal(email)
+    return user
+}
+
+const getDatabaseUserById = async (userId) => {
+    const res = await requestDb('select * from users where id =?', [ userId])
+    expect(res).to.be.instanceOf(Array)
+    if (res.length === 0)
+        return null // user not found with this id
+    const user = res[0]
+    return user
+}
+
+
+
+const userPublicProperties = [
+    'id', 'email', 'firstname', 'lastname', 'state', 'lastUse', 'administrator', 'companyId'
+]
+
+export {
+    createUserAccount,
+    deleteUserAccount,
+    getDatabaseUserByEmail,
+    getDatabaseUserById,
+    userPublicProperties 
+}
--- api-lib/test/util.js
+++ api-lib/test/util.js
@@ -1,11 +1,18 @@
 'use strict'
 import dotenv from 'dotenv'
+import promise_mysql from 'promise-mysql'
 
 import ComaintBackendApi from '../src/ComaintBackendApi.js'
 
 dotenv.config({ path: './test/.env' })
 
 let accountData = null
+let db = null
+let dbHost
+let dbPort
+let dbDatabase
+let dbUser
+let dbPassword
 
 const accountSerializeFunction = (data) => {
     if (data === undefined)
@@ -23,4 +30,47 @@ const initializeApi = () => {
     return api
 }
 
-export { initializeApi }
+
+const connectDb = async () => {
+    dbHost = process.env.DB_HOST || 'localhost'
+    dbPort = process.env.DB_PORT || 3306
+    dbDatabase = process.env.DB_DATABASE || 'db_comaint'
+    dbUser = process.env.DB_USER || 'comaint'
+    dbPassword = process.env.DB_PASSWORD
+
+    if (dbPassword === undefined)
+        throw new Error(`Parameter «DB_PASSWORD» not found in «${CONF_FILE}»`)
+
+    db = await promise_mysql.createConnection({
+        host: dbHost,
+        port: dbPort,
+        database: dbDatabase,
+        user: dbUser,
+        password: dbPassword
+    })
+    if (db.code)
+        throw new Error(`Can't connect database`)
+}
+
+const disconnectDb = async () => {
+    if (db === null)
+        return
+    db.end()
+    db = null
+}
+
+const requestDb = async (sqlQuery, sqlValues = []) => {
+    if (db === null)
+        await connectDb()
+    const result = await db.query(sqlQuery, sqlValues);
+    if (result.code)
+        throw new Error(`SQL error : ${result.code}`)
+    return result
+}
+
+export {
+    initializeApi,
+    connectDb,
+    disconnectDb,
+    requestDb,
+}
