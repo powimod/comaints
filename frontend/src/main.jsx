e4c9a281232faac5dfd20554471e0cb266cc43ba
Dominique Parisot <powimod@gmail.com>
2024-11-03 18:37:54 +0100

    Simplify main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
 import { Provider as ReduxProvider } from 'react-redux'
+import { RouterProvider } from 'react-router-dom'
 import './i18n.js'
 
 
 import DialogProvider from  './components/dialog/DialogContext.jsx'
-import ComaintContextProvider, { useComaintContext } from './ComaintContext.jsx'
+import ComaintContextProvider from './ComaintContext.jsx'
 
-import { RouterProvider } from 'react-router-dom'
 import initializeStore from './store.js'
 import createRouter from './router'
 
